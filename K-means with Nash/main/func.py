5aab7a3d57ea790627e6ea0927380f63c6cd64c6
psh250092 <53813106+psh250092@users.noreply.github.com>
2022-11-08 12:09:58 +0900

    mf

--- K-means with Nash/main/func.py
+++ K-means with Nash/main/func.py
@@ -1,8 +1,23 @@
 import pandas as pd
 from sklearn.cluster import KMeans
 import numpy
+import matplotlib.pyplot as plt
 
 
+def showColormap(payoffMatrix):
+    fig = plt.figure(figsize=(6, 3.2))
+    ax = fig.add_subplot(111)
+    ax.set_title('colorMap')
+    plt.imshow(payoffMatrix)
+    ax.set_aspect('equal')
+
+    cax = fig.add_axes([0.12, 0.1, 0.78, 0.8])
+    cax.get_xaxis().set_visible(False)
+    cax.get_yaxis().set_visible(False)
+    cax.patch.set_alpha(0)
+    cax.set_frame_on(False)
+    plt.colorbar(orientation='vertical')
+    plt.show()
 
 # clustering
 # @copyright by Sungmin-Ha
